f7a87c200ee2c4103c4beccd21d3e4ca12275129
Harry San Juan <harrycsanjuan@gmail.com>
2024-08-21 18:18:21 -0400

    compelete lab

--- server/app.py
+++ server/app.py
@@ -17,3 +17,34 @@ def index():
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+
+
+# This is a test-driven lab.
+
+# Run pipenv install to create your virtual environment and pipenv shell to enter the virtual environment.
+
+# pipenv install && pipenv shell
+# This project has starter code for a couple of models, Author and Post. To get create the database from the initial migration, run:
+
+#  cd server
+#  flask db upgrade
+#  python seed.py
+# Basic Validations
+# Add validators to the Author and Post models such that:
+
+# All authors have a name.
+# No two authors have the same name.
+# Author phone numbers are exactly ten digits.
+# Post content is at least 250 characters long.
+# Post summary is a maximum of 250 characters.
+# Post category is either Fiction or Non-Fiction.
+# Post title is sufficiently clickbait-y and must contain one of the following:
+# "Won't Believe"
+# "Secret"
+# "Top"
+# "Guess"
+# You should not need to run another migration, unless you altered model constraints.
+
+# Run pytest -x to run your tests. Use these instructions and pytest's error messages to complete your work in the server/ folder.
--- server/models.py
+++ server/models.py
@@ -11,7 +11,28 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    @validates('name')
+    def validates_names(self, key, name_input):
+        if len(name_input) == 0:
+            raise ValueError('Authors must have a name')
+        
+        name_validation = Author.query.filter_by(name=name_input).all()
+
+        if name_validation:
+            raise ValueError('No two authors must have the same name.')
+        
+        return name_input
+
+    @validates('phone_number')
+    def validates_phone_number(self, key, phone_num):
+        cleaned_num = ''.join([integer for integer in phone_num if integer.isdigit()])
+
+        if len(cleaned_num) != 10:
+            raise ValueError('Author phone numbers must be exactly 10 digits')
+        
+        return phone_num
+
+
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,7 +48,38 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    @validates('content')
+    def validates_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Post content must be at least 250 characters long.')
+        return content
+        
+    @validates('summary')
+    def validates_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('Post summary has a maximum of 250 characters.')
+        return summary
+        
+    @validates('category')
+    def validates_category(self, key, category):
+        categories = ['Fiction', 'Non-Fiction']
+        if category not in categories:
+            raise ValueError('Post category must either be Fiction or Non-Fiction.')
+        return category
+    
+    @validates('title')
+    def validates_title(self, key, title):
+        if len(title) == 0:
+            raise ValueError("Each post must have a title")
+        
+        click_bait = ['Won\'t Believe', 'Secret', 'Top', 'Guess']
+
+        click_bait_test = [bait for bait in click_bait if bait in title]
+        
+        if not click_bait_test:
+            raise ValueError('Title must be sufficiently clickbait-y')
+
+        return title
 
 
     def __repr__(self):
